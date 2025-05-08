9db83db3bd4eb29ca126813f9841764418bd1d12
elwood65 <fenotti65@gmail.com>
2025-05-08 11:22:45 -0300

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -12,8 +12,8 @@ import time
 
 # Constants
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://elwood65-vadase.hf.space"
+PASSWORD = "shadow65"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
