237c2d03f105a33abbfaa05f9cbb35b244d7cf93
elwood65 <fenotti65@gmail.com>
2025-05-08 11:40:53 -0300

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -8,8 +8,8 @@ PASSWORD = "shadow65"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
-USR = "elwood65"
-BRANCH = "https://github.com/elwood65/OMGPRX"
+USR = "https://github.com/elwood65/OMGPRX"
+BRANCH = "main"
 
 OUTPUT_FILE = "channels_italy.m3u8"
 BASE_URL = "https://vavoo.to"
