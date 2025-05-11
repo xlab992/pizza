498e95287946d3f0e59d580ae3fdd1c9bc224c5e
xlab992 <demon_raz92@hotmail.it>
2025-05-11 17:08:05 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,8 +13,8 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 # Constants
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://huggingface.co/spaces/xlab992/bb"
+PASSWORD = "gf4"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
