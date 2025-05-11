9a45d0b786042d69c8d8e0a7f0fa9475a55b3a53
xlab992 <demon_raz92@hotmail.it>
2025-05-11 17:09:52 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -3,13 +3,13 @@ import json
 import re
 import os
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://huggingface.co/spaces/xlab992/bb"
+PASSWORD = "gf4"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
-USR = "TUO_USER_GITHUB"
-REPO = "TUO_BRANCH_GITHUB"
+USR = "xlab992"
+REPO = "pizza"
 
 OUTPUT_FILE = "channels_italy.m3u8"
 BASE_URL = "https://vavoo.to"
